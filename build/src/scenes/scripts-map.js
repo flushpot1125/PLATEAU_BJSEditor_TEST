3d4da7368944c8315f1a02b2c9bddef9f7e34359
flushpot1125 <crescentblue1122@gmail.com>
2021-07-17 18:10:02 +0900

    Initial commit

---
+++ build/src/scenes/scripts-map.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Defines the map of all available scripts in the project.
+ */
+exports.scriptsMap = {
+    "src/scenes/scene/WebXRConfiguration.ts": require("./scene/WebXRConfiguration"),
+};
+//# sourceMappingURL=scripts-map.js.map
